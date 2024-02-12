4289cd50d7a0e3de1eae270645d17bb0a51a4ccb
Aldino Efendi <aldino.efendi@ist.id>
2024-02-13 04:01:17 +0700

    feat: update code and add command

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
-require("dotenv").config();
-const { DISCORD_BOT_TOKEN, PREFIX } = process.env;
-const { Client, MessageEmbed } = require("discord.js");
+const { DISCORD_BOT_TOKEN, PREFIX } = process.env
+import { Client, GatewayIntentBits, Partials, EmbedBuilder } from "discord.js"
 
 const client = new Client({
-  partials: ["MESSAGE", "REACTION"],
+  intents: [
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildModeration,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [Partials.Channel],
 });
+
 client.login(DISCORD_BOT_TOKEN);
 
-client.on("ready", () => {
-  console.log(`${client.user.tag} has logged in`);
-});
+client.on("ready", () => console.log(`✅ ${client.user.tag} has logged in`));
 
-/**
- ** Bot Reaction
- */
+//* Bot Reaction *//
 client.on("messageReactionAdd", async (reaction, user) => {
   const { name } = reaction.emoji;
   const member = reaction.message.guild.members.cache.get(user.id);
@@ -44,9 +47,7 @@ client.on("messageReactionAdd", async (reaction, user) => {
   }
 });
 
-/**
- ** Bot Reaction Remove
- */
+//* Bot Reaction Remove *//
 client.on("messageReactionRemove", async (reaction, user) => {
   const { name } = reaction.emoji;
   const member = reaction.message.guild.members.cache.get(user.id);
@@ -77,39 +78,38 @@ client.on("messageReactionRemove", async (reaction, user) => {
   }
 });
 
-/**
- ** Bot Command
- */
-const kickCMD = "`;kick <mention the user>`";
-const tossCMD = "`;toss`";
-const warnCMD = "`;warn <mention the user>`";
-const hiCMD = "`;hi`";
+//* Bot Command *//
+
+const kickCMD = `${PREFIX}kick <mention the user>`;
+const tossCMD = `${PREFIX}toss`;
+const warnCMD = `${PREFIX}warn <mention the user>`;
+const hiCMD = `${PREFIX}hi`;
+const roastCMD = `${PREFIX}roast <mention the user>`
 
 const helpMessage = `
 Command list that you can use:
 
-${tossCMD} to start the rolldice,
-${kickCMD} to kick user,
-${hiCMD} to say hello,
-${warnCMD} to warning an user 
+- \`${tossCMD}\` to start the rolldice
+- \`${kickCMD}\` to kick user
+- \`${hiCMD}\` to say hello
+- \`${warnCMD}\` to warning an user
+- \`${roastCMD}\` to roasting an user
 `;
-
-client.on("message", async (message) => {
+client.on('messageCreate', async (message) => {
   if (message.content.startsWith(PREFIX)) {
     const [CMD_NAME, ...args] = message.content
       .toLowerCase()
       .trim()
       .substring(PREFIX.length)
-      .split(/\s+/);
-
+      .split(/\s+/)
+    
     const channelName = message.guild.name;
     const memberName = message.member;
     const mentionedUser = message.mentions.users.first();
-    const member = message.guild.member(mentionedUser);
 
     switch (CMD_NAME) {
       case "hi":
-        message.channel.send(
+        message.reply(
           `Hi ${memberName}, you can check other command by typing ***;help***`
         );
         break;
@@ -131,57 +131,75 @@ client.on("message", async (message) => {
         break;
 
       case "kick":
-        if (!message.member.hasPermission("KICK_MEMBERS")) {
+        if (!message.member.hasPermission("KICK_MEMBERS")) 
           return message.reply("You roles cannot kick someone in this channel");
-        }
 
         if (!mentionedUser)
           return message.reply("You didn't mention the user to kick!");
 
-        if (member) {
-          try {
-            await member.kick();
-            return message.channel.send(
-              `Yay, ${member} has been kicked from this channel`
-            );
-          } catch (error) {
-            console.log(error);
-            return message.channel.send(
-              "User was not found or I don't have permission to kick the mentioned user"
-            );
-          }
+        try {
+          const member = await message.guild.members.fetch(mentionedUser);
+          await member.kick();
+          return message.channel.send(
+            `Yay, ${member} has been kicked from this channel`
+          );
+        } catch (error) {
+          console.error(error)
+          message.channel.send(
+            "User was not found or I don't have permission to kick the mentioned user"
+          );
         }
         break;
 
       case "ban":
-        if (!message.member.hasPermission("BAN_MEMBERS")) {
+        if (!message.member.hasPermission("BAN_MEMBERS")) 
           return message.reply("You roles cannot ban someone in this channel");
-        }
 
         if (!mentionedUser)
           return message.reply("You didn't mention the user to ban!");
 
-        if (member) {
-          try {
-            await member.ban();
-            return message.channel.send(
-              `Yay, ${member} has been banned from this channel`
-            );
-          } catch (error) {
-            console.log(error);
-            return message.channel.send(
-              "User was not found or I don't have permission to ban the mentioned user"
-            );
-          }
+        try {
+          const member = await message.guild.members.fetch(mentionedUser);
+          await member.ban();
+          return message.channel.send(
+            `Bye bye ${member}, you has has been banned from ${channelName} server`
+          );
+        } catch (error) {
+          console.error(error)
+          message.channel.send(
+            "User was not found or I don't have permission to ban the mentioned user"
+          );
         }
-        break;
+        break
+
+      case 'roast':
+        if (!mentionedUser)
+          return message.reply('Mention orangnya cok, gimana mau roast tapi orgnya tak ada')
+
+        try {
+          const member = await message.guild.members.fetch(mentionedUser);
+          const embed = new EmbedBuilder()
+          .setTitle("Anjay bot commands")
+          .setColor(0xff0000)
+          .setDescription(
+            `Jalan jalan ke pantai
+            karena macet lewat jalan tol
+            hei ${member} yang lagi makan petai
+            muka kau kayak k*nt*l`
+          );
+          return message.channel.send({ embeds: [embed] });
+        } catch (error) {
+          console.error(error)
+          message.channel.send('Something went wrong, I can feel it')
+        }
+        break
 
       case "help":
-        const embed = new MessageEmbed()
+        const embed = new EmbedBuilder()
           .setTitle("Anjay bot commands")
           .setColor(0xff0000)
           .setDescription(helpMessage);
-        message.channel.send(embed);
+        message.channel.send({ embeds: [embed] });
         break;
       default:
         return message.channel.send(
@@ -189,11 +207,9 @@ client.on("message", async (message) => {
         );
     }
   }
-});
+})
 
-/**
- ** Bot Welcome
- */
+//* Bot Welcome *//
 client.on("guildMemberAdd", (member) => {
   const channelId = "749657502114906146";
   const serverName = member.guild.name;
